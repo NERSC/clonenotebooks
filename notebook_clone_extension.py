73518f5963bcd9a8ddf6fbc6a603817b5ac74376
William Krinsman <krinsman@berkeley.edu>
2019-06-06 16:14:58 -0700

    Minor naming convention changes to make the purpose of the extension more obvious and to leave room open for e.g. cloning from GitHub.

--- notebook_clone_extension.py
+++ notebook_clone_extension.py
@@ -20,19 +20,19 @@ def load_jupyter_server_extension(nb_server_app):
 
     # This class is defined in line so it can close over contents_manager.
 
-    class CloneHandler(IPythonHandler):
+    class LocalCloneHandler(IPythonHandler):
         def get(self):
             # This is similar to notebook.contents.manager.ContentsManager.copy
-            # but it (1) assumes the copy_from is on the filesystem not some
+            # but it (1) assumes the clone_from is on the filesystem not some
             # non-file-based ContentManager implementation and (2) is able to
-            # copy files from outside of ("above") the notebook server's root
+            # clone files from outside of ("above") the notebook server's root
             # directory.
-            copy_from = self.get_argument('copy_from')
-            copy_to = "/"  # root directory of notebook server
-            self.log.info("Copying %s to %s", copy_from, copy_to)
-            if not os.path.isfile(copy_from):
-                raise web.HTTPError(400, "No such file: %s" % copy_from)
-            with open(copy_from, 'r') as f:
+            clone_from = self.get_argument('clone_from')
+            clone_to = "/"  # root directory of notebook server
+            self.log.info("Cloning %s to %s", clone_from, clone_to)
+            if not os.path.isfile(clone_from):
+                raise web.HTTPError(400, "No such file: %s" % clone_from)
+            with open(clone_from, 'r') as f:
                 content = json.load(f)
             now = datetime.now()
             model = {
@@ -43,15 +43,16 @@ def load_jupyter_server_extension(nb_server_app):
                 'mimetype': None,
                 'type': 'notebook',
                 'writable': True}
-            name = copy_pat.sub(u'.', os.path.basename(copy_from))
-            to_name = contents_manager.increment_filename(name, copy_to, insert='-Copy')
-            full_copy_to = u'{0}/{1}'.format(copy_to, to_name)
-            contents_manager.save(model, full_copy_to)
-            # Redirect to the copied notebook
+            name = copy_pat.sub(u'.', os.path.basename(clone_from))
+            to_name = contents_manager.increment_filename(name, clone_to, insert='-Copy')
+            full_clone_to = u'{0}/{1}'.format(clone_to, to_name)
+            contents_manager.save(model, full_clone_to)
+            # Redirect to the cloned notebook
             # in JupyterLab's single-document mode.
-            self.redirect(url_path_join('lab', 'tree', full_copy_to))
+            self.redirect(url_path_join('lab', 'tree', full_clone_to))
+
 
     host_pattern = '.*$'
-    route_pattern = url_path_join(web_app.settings['base_url'], '/clone')
+    route_pattern = url_path_join(web_app.settings['base_url'], '/local_clone')
 
-    web_app.add_handlers(host_pattern, [(route_pattern, CloneHandler)])
+    web_app.add_handlers(host_pattern, [(route_pattern, LocalCloneHandler)])
