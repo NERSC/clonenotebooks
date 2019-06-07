7a3ae5ca80972865284c031932c325101654daec
William Krinsman <krinsman@berkeley.edu>
2019-06-07 10:41:52 -0700

    An actual 80% working GitHub clone handler prototype.

--- notebook_clone_extension.py
+++ notebook_clone_extension.py
@@ -1,15 +1,20 @@
 from datetime import datetime
 import json
 import os.path
+from io import StringIO
 
 from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
 from notebook.services.contents.manager import copy_pat
 from tornado import web
+from tornado.escape import url_unescape
+from tornado import gen
+from tornado import httpclient
 
 from contextlib import contextmanager
-from nbviewer.providers.github import AsyncGitHubClient
+from nbviewer.providers.github.client import AsyncGitHubClient
 from nbviewer.utils import response_text
+from nbviewer.utils import base64_decode
 
 def load_jupyter_server_extension(nb_server_app):
     """
@@ -54,7 +59,16 @@ def load_jupyter_server_extension(nb_server_app):
             # in JupyterLab's single-document mode.
             self.redirect(url_path_join('lab', 'tree', full_clone_to))
 
-    class GithubCloneHandler(IPythonHandler):
+    class GitHubCloneHandler(IPythonHandler):
+        client = httpclient.AsyncHTTPClient()
+
+        @property 
+        def github_client(self):
+            """Create an upgraded GitHub API client from the HTTP client"""
+            if getattr(self, "_github_client", None) is None:
+                self._github_client = AsyncGitHubClient(self.client)
+            return self._github_client
+
         @contextmanager
         def catch_client_error(self):
             """context manager for catching httpclient errors
@@ -68,22 +82,29 @@ def load_jupyter_server_extension(nb_server_app):
             except socket.error as e:
                 raise web.HTTPError(404, str(e))
         
+        @gen.coroutine
         def get(self):
             # This is similar to notebook.contents.manager.ContentsManager.copy
             # but it (1) assumes the clone_from is on the filesystem not some
             # non-file-based ContentManager implementation and (2) is able to
             # clone files from outside of ("above") the notebook server's root
             # directory.
-            clone_from = self.get_argument('clone_from')
+            clone_from = url_unescape(self.get_argument('clone_from'))
             clone_to = "/"  # root directory of notebook server
             self.log.info("Cloning notebook on GitHub found at %s to %s", clone_from, clone_to)
 
-            split_clone_from = clone_from.split('/', 2)
-            user = split_clone_from[0]
-            repo = split_clone_from[1]
-            path = split_clone_from[2]
+            clone_from = clone_from.split('/', 3)
+            user = clone_from[0]
+            self.log.info("\nValue of user is: %s\n" % user)
+            repo = clone_from[1]
+            self.log.info("\nValue of repo is: %s\n" % repo)
+            path = clone_from[2]
+            self.log.info("\nValue of path is: %s\n" % path)
+            ref  = clone_from[3]
+            self.log.info("\nValue of ref is: %s\n"  % ref)
             
             with self.catch_client_error():
+                self.log.info("\nGot to with self.catch_client error -- does it fail here?\n")
                 tree_entry = yield self.github_client.get_tree_entry(
                     user, repo, path=url_unescape(path), ref=ref
                     )
@@ -107,7 +128,11 @@ def load_jupyter_server_extension(nb_server_app):
                     nbjson = filedata.decode('utf-8')
                 else:
                     nbjson = filedata
+            except Exception as e:
+                app_log.error("Failed to decode notebook: %s", path, exc_info=True)
+                raise web.HTTPError(400)
 
+            nbjson = json.load(StringIO(nbjson))
             now = datetime.now()
             model = {
                 'content': nbjson,
@@ -117,7 +142,7 @@ def load_jupyter_server_extension(nb_server_app):
                 'mimetype': None,
                 'type': 'notebook',
                 'writable': True}
-            name = copy_pat.sub(u'.', os.path.basename(clone_from))
+            name = copy_pat.sub(u'.', os.path.basename(path))
             to_name = contents_manager.increment_filename(name, clone_to, insert='-Copy')
             full_clone_to = u'{0}/{1}'.format(clone_to, to_name)
             contents_manager.save(model, full_clone_to)
@@ -127,6 +152,8 @@ def load_jupyter_server_extension(nb_server_app):
 
 
     host_pattern = '.*$'
-    route_pattern = url_path_join(web_app.settings['base_url'], '/github_clone')
+    local_route_pattern  = url_path_join(web_app.settings['base_url'], '/local_clone')
+    github_route_pattern = url_path_join(web_app.settings['base_url'], '/github_clone')
 
-    web_app.add_handlers(host_pattern, [(route_pattern, LocalCloneHandler)])
+    web_app.add_handlers(host_pattern, [(local_route_pattern, LocalCloneHandler),
+                                        (github_route_pattern, GitHubCloneHandler)])
