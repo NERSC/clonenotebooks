a3ff04e6e5416e03a9fb0d62b7f701c5dc4369b2
William Krinsman <krinsman@berkeley.edu>
2019-07-02 10:54:06 -0700

    Trying to reorganize repo so the distinction between 'cloners' NB Server Extension and 'renderers' NBViewer extension is more apparent.

---
+++ clonenotebooks/renderers/__init__.py
@@ -0,0 +1,6 @@
+from .renderers import URLRenderingHandler
+from .renderers import LocalRenderingHandler
+from .renderers import GitHubBlobRenderingHandler
+from .renderers import GitHubTreeRenderingHandler
+from .renderers import GistRenderingHandler
+from .renderers import UserGistsRenderingHandler
