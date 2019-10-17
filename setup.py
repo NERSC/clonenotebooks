adda76577b82cc7a07029c52081bf4f894deb0d7
William Krinsman <krinsman@berkeley.edu>
2019-10-17 10:47:23 -0700

    Attempted patch

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='clonenotebooks',
-      packages=['clonenotebooks'],
+      packages=['clonenotebooks.cloners', 'clonenotebooks.renderers'],
       include_package_data=True,
       )
