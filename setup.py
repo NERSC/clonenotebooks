7e83fd3a1781c94090dfdfafe6db343d0b27a556
William Krinsman <krinsman@berkeley.edu>
2019-11-06 14:03:22 -0800

    Attempted patch for pip installs

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='clonenotebooks',
-      packages=['clonenotebooks.cloners', 'clonenotebooks.renderers'],
+      packages=['clonenotebooks', 'clonenotebooks.cloners', 'clonenotebooks.renderers'],
       include_package_data=True,
       )
